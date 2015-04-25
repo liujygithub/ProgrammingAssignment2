9ae199a2dc7e1fdcc7b17fa15551dedadde67a78
liujygithub <liujingyingpku@163.com>
2015-04-25 11:01:00 +0800

    create two functions to calculate inverse matrix without repeat and write some comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+###There are two functions below, which are to cache inverse matrix without calculating repeatedly
 
-## Write a short comment describing this function
+## This is the first function that makes a special matrix
+##The sepcial matrix is to cache inverse matrix 
+
 
 makeCacheMatrix <- function(x = matrix()) {
+	m<-NULL
+	set<-function(y){
+		x<<-y
+		m<<-NULL
+	}
+	get<-function()x
+	setCacheMatri<-function(solve) m<<-solve
+	getCacheMatri<-function()m
+	list(set=set, get=get, setCacheMatri=setCacheMatri, getCacheMatri=getCacheMatri)
 
 }
 
+## This is the second function
+## It is to judge whether the inverse has been calculated and then decide
+## if it is neccessary to calculate again
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+	m<-x$getCacheMatri()
+	if(!is.null(m)){
+		message("getting cached data")
+		return(m)
+	}
+	data <-x$get()
+	m<-solve(data,...)
+	x$setCacheMatri(m)
+	m
+
+
